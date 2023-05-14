69a56e9cead60e33ff4c0d88399bcdc0d35404d4
yuri <yuri@example.com>
2023-05-14 16:55:05 +0900

    39. タスク削除後の並び替えのバグ修正

--- src/components/task/input/TaskAddInput.jsx
+++ src/components/task/input/TaskAddInput.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+// https://www.npmjs.com/package/uuid/v/8.3.0
+import { v4 as uuid } from "uuid";
 
 export const TaskAddInput = ({
   inputText,
@@ -7,6 +9,8 @@ export const TaskAddInput = ({
   setTaskList,
 }) => {
   const handleSubmit = (e) => {
+    // ユニークなIdを生成
+    const taskId = uuid();
     e.preventDefault();
     if (inputText === "") return;
 
@@ -14,8 +18,8 @@ export const TaskAddInput = ({
     setTaskList([
       ...taskList,
       {
-        id: taskList.length,
-        draggableId: `task${taskList.length}`,
+        id: taskId,
+        draggableId: `task${taskId}`,
         text: inputText,
       },
     ]);
