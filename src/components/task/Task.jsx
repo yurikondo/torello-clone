04960f8b9cdc4619d32730b2901ad14cf5dfbd21
yuri <yuri@example.com>
2023-05-14 16:43:33 +0900

    38. ドラッグ＆ドロップ後にカードを並び替えよう

--- src/components/task/Task.jsx
+++ src/components/task/Task.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 
-export const Task = ({ task, taskList, setTaskList }) => {
+export const Task = ({ task, taskList, setTaskList, index }) => {
   const handleDelete = (id) => {
     // task.id !== idがtrueの値だけtaskListに戻す＝削除したタスク以外を保存
     setTaskList(taskList.filter((task) => task.id !== id));
   };
 
   return (
-    <Draggable index={task.id} draggableId={task.draggableId}>
+    <Draggable index={index} draggableId={task.draggableId}>
       {(provided) => (
         <div
           className="taskBox"
--- src/components/task/Tasks.jsx
+++ src/components/task/Tasks.jsx
@@ -2,16 +2,31 @@ import React from "react";
 import { Task } from "./Task";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
+const reorder = (taskList, startIndex, endIndex) => {
+  // タスクを並び替える。レッスン38
+  //動かしたタスクを削除
+  const remove = taskList.splice(startIndex, 1); //[2,3]
+  //動かしたタスクを移動さきに挿入
+  taskList.splice(endIndex, 0, remove[0]); //[1,2,3]
+};
+
 export const Tasks = ({ taskList, setTaskList }) => {
+  const handleDragEnd = (result) => {
+    reorder(taskList, result.source.index, result.destination.index);
+    setTaskList(taskList);
+  };
+
   return (
     <div>
-      <DragDropContext>
+      {/* onDragEndはドラッグが終わった時 */}
+      <DragDropContext onDragEnd={handleDragEnd}>
         <Droppable droppableId="droppable">
           {(provided) => (
             <div {...provided.droppableProps} ref={provided.innerRef}>
-              {taskList.map((task) => (
+              {taskList.map((task, index) => (
                 <div key={task.id}>
                   <Task
+                    index={index}
                     task={task}
                     taskList={taskList}
                     setTaskList={setTaskList}
