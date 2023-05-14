9054fb888d0be29bb820e8136ca2facd22a17dec
yuri <yuri@example.com>
2023-05-14 10:47:31 +0900

    27. TaskCardsコンポーネントを作成しよう

---
+++ src/components/task/TaskCards.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { TaskCard } from "./TaskCard";
+import { AddTaskCardBtn } from "../button/AddTaskCardBtn";
+
+const TaskCards = () => {
+  return (
+    <div>
+      <TaskCard />
+      <AddTaskCardBtn />
+    </div>
+  );
+};
+
+export default TaskCards;
