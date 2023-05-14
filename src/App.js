bb9dc9b091da84ebcfc2d5fcf07391ead230c1ae
yuri <yuri@example.com>
2023-05-14 10:35:27 +0900

    25. Headerコンポーネントを作成しよう

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Header from "./components/header/Header";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div><header><h1>Simple Torello</h1></header></div>
+  )
+}
+
+export default Header
